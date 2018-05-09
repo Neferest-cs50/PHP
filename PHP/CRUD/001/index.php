67e55a20640544f9874be47371add5589f3d08b9
abdellah-saq <abdellah_saq@hotmail.com>
2018-05-09 10:25:11 +0100

    base

--- PHP/CRUD/001/index.php
+++ PHP/CRUD/001/index.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Medharik - PHP CRUD</title>
+<?php include 'inc/header.php'; ?>
+      
+  <h1>Hello, world!</h1>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-  </head>
-
-
-  <body>
-    <h1>Hello, world!</h1>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php include 'inc/footer.php'; ?>
